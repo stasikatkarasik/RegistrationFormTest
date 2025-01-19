e79bff33b93a2b97f84b693e745072e5d92027c7
Стася <stasikatkarasik@yandex.ru>
2025-01-19 21:23:35 +0300

    delited unused

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
